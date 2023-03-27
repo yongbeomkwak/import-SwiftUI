eace287febcb32658653c3ca88604a22ea0ea980
Hamp <yongbeomkwak@gamil.com>
2023-03-28 08:48:03 +0900

    :tada: 알람 프로젝트 생성

---
+++ SwiftUI/KayleAlarm/KayleAlarm/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KayleAlarm
+//
+//  Created by yongbeomkwak on 2023/03/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/KayleAlarm/KayleAlarm/KayleAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  KayleAlarmApp.swift
+//  KayleAlarm
+//
+//  Created by yongbeomkwak on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct KayleAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
