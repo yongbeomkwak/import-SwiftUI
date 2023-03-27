62cd2742cb40daa9ab31515000a79c3c2046416c
Hamp <yongbeomkwak@gamil.com>
2023-03-27 20:45:22 +0900

    :poop:

--- SwiftUI/KayleAlarm/KayleAlarm/Source/ContentView.swift
+++ SwiftUI/KayleAlarm/KayleAlarm/Source/ContentView.swift
@@ -10,11 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            Image("Timer")
+                
             Text("Hello, world!")
-                .foregroundColor()
+            
                 
                 
         }
