c9ef52b272cc01babfaf4b442faaed03118930b0
snack <kwansiknam@gmail.com>
2023-03-28 23:57:40 +0900

    💄 :: Color 익스텐션 생성

---
+++ SwiftUI/Snack/SnackAlarm/SnackAlarm/Resource/Util/Extension+Color.swift
@@ -0,0 +1,20 @@
+//
+//  Extension+Color.swift
+//  SnackAlarm
+//
+//  Created by 관식 on 2023/03/28.
+//
+
+import SwiftUI
+import Foundation
+
+extension Color {
+    
+    static let bgColor: Self = .black
+    static let mainTextColor: Self = .white
+    static let modalBgColor: Self = Color("Modal")
+    static let subTextColor: Self = Color("SubText")
+    static let tableColor: Self = Color("Table")
+    static let highlightColor: Self = Color("Highlight")
+
+}
