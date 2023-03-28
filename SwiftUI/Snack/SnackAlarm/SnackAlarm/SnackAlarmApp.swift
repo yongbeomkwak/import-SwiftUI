4a1e5c381802545a22d26d9afd4bcb61f840802e
snack <kwansiknam@gmail.com>
2023-03-28 23:31:04 +0900

    :tada: :: 알람 프로젝트 생성

---
+++ SwiftUI/Snack/SnackAlarm/SnackAlarm/SnackAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackAlarmApp.swift
+//  SnackAlarm
+//
+//  Created by 관식 on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct SnackAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
