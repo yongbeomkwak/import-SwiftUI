6c5dd5dcbddd8133b66953bf5ec431699848b2ba
snack <kwansiknam@gmail.com>
2023-03-28 23:45:45 +0900

    💄 :: Font 추가 및 익스텐션 생성

---
+++ SwiftUI/Snack/SnackAlarm/SnackAlarm/Resource/Util/Extension+Font.swift
@@ -0,0 +1,19 @@
+//
+//  Extension+Font.swift
+//  SnackAlarm
+//
+//  Created by 관식 on 2023/03/28.
+//
+
+import Foundation
+import SwiftUI
+
+//SFRounded-Ultralight
+
+extension Font {
+    
+    static func sFRounded(_ size: CGFloat = 10) -> Font {
+        return Font.custom("SFRounded-Ultralight", size: size)
+    }
+    
+}
