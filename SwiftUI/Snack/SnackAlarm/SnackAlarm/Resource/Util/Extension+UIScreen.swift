4298214dbd38ddef5fddd3b43a7938d046d58712
snack <kwansiknam@gmail.com>
2023-03-29 00:19:55 +0900

    💄 :: UIScreen 익스텐션 생성

---
+++ SwiftUI/Snack/SnackAlarm/SnackAlarm/Resource/Util/Extension+UIScreen.swift
@@ -0,0 +1,16 @@
+//
+//  Extension+UIScreen.swift
+//  SnackAlarm
+//
+//  Created by 관식 on 2023/03/29.
+//
+
+import SwiftUI
+import Foundation
+
+extension UIScreen {
+    
+    static let width = UIScreen.main.bounds.width
+    static let height = UIScreen.main.bounds.height
+    
+}
