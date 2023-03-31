9a57381554e8ed64c8be0cc5e73b6aef959da99a
Hamp <yongbeomkwak@gmail.com>
2023-03-31 21:10:52 +0900

    :recylce: ::  zPosition을 Layer 구조체 변수 이용하여 재정의

---
+++ SpriteKit/Source/FlappyBirdLike/FlappyBirdLike/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  FlappyBirdLike
+//
+//  Created by yongbeomkwak on 2023/03/31.
+//
+
+import Foundation
+
+struct Layer {
+    
+    static let sky: CGFloat = 1
+    static let pipe: CGFloat = 2
+    static let land: CGFloat = 3
+    static let ceiling: CGFloat = 4
+    static let bird: CGFloat = 5
+    
+}
