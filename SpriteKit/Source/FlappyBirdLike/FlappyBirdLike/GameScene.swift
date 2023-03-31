eb5c518c096782e490bb74e280887c46348e1b4e
Hamp <yongbeomkwak@gmail.com>
2023-03-31 17:00:45 +0900

    :lipstick: :: 이미지 회전 및 반전
    - .xScale = -1 // x방향으로 180도 회전 (좌우 반전)
    - .zRotation = .pi // 상하 반전

--- SpriteKit/Source/FlappyBirdLike/FlappyBirdLike/GameScene.swift
+++ SpriteKit/Source/FlappyBirdLike/FlappyBirdLike/GameScene.swift
@@ -13,10 +13,11 @@ class GameScene: SKScene {
         //초기화 진행
         
         let width = self.size.width
+        let height = self.size.height
         
         let land = SKSpriteNode(imageNamed: "land")
         land.position = CGPoint(x: width/2, y: 50)
-        land.zPosition = 3 // 숫자가 클수록 화면 뒤쪽으로 배치 
+        land.zPosition = 3 // 숫자가 클수록 화면 뒤쪽으로 배치
         self.addChild(land)
         
         let sky = SKSpriteNode(imageNamed: "sky")
@@ -27,6 +28,8 @@ class GameScene: SKScene {
         let bird = SKSpriteNode(imageNamed: "bird")
         bird.position = CGPoint(x: width/2, y: 200)
         bird.zPosition = 2
+        
+        
         self.addChild(bird)
         
         let ceiling = SKSpriteNode(imageNamed: "ceiling")
@@ -34,6 +37,18 @@ class GameScene: SKScene {
         ceiling.zPosition = 3
         self.addChild(ceiling)
         
+        let pipeUp = SKSpriteNode(imageNamed: "pipe")
+        pipeUp.position = CGPoint(x: width/2, y: 100)
+        pipeUp.zPosition = 2
+        self.addChild(pipeUp)
+        
+        let pipeDown = SKSpriteNode(imageNamed: "pipe")
+        pipeDown.position = CGPoint(x: width/2, y: height)
+        pipeDown.zPosition = 2
+        pipeDown.xScale = -1 // x방향으로 180도 회전 (좌우 반전)
+        pipeDown.zRotation = .pi // 상하 반전
+        
+        self.addChild(pipeDown)
         
     }
 
