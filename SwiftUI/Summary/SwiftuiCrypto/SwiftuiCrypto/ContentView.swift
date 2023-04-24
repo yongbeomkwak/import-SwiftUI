162f2686437c80d6a85031040659958a4db61244
Hamp <yongbeomkwak@gmail.com>
2023-04-24 22:20:17 +0900

    :tada: :: crypto 클론 코딩 시작

---
+++ SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftuiCrypto
+//
+//  Created by yongbeomkwak on 2023/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/SwiftuiCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiCryptoApp.swift
+//  SwiftuiCrypto
+//
+//  Created by yongbeomkwak on 2023/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
