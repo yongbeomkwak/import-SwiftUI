e38d119d1249e8dd8fa5ab0518f49f0bda3ea8f6
Hamp <yongbeomkwak@gmail.com>
2023-04-28 08:56:32 +0900

    :sparkles: :: 코인데이터 서비스 구현

--- SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Services/CoinDataService.swift
+++ SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Services/CoinDataService.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import Combine
 
 class CoinDataService {
     
     @Published var allCoins: [CoinModel] = []
+    var coinSubscription: AnyCancellable?
     
     
     init(){
@@ -22,8 +24,36 @@ class CoinDataService {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else {return}
         
     
-        URLSession.shared.dataTaskPublisher(for: url)
+        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap{ (output) -> Data in
+                guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200
+                        && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                
+                return output.data
+                
+            }
+            .receive(on:DispatchQueue.main)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink(receiveCompletion: { (completion) in
+                
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+                
+                
+            }, receiveValue: { [weak self] coins in
+                guard let self else  {return}
+                self.allCoins  = coins
+                self.coinSubscription?.cancel()
+                
+            })
+           
         
         
     }
