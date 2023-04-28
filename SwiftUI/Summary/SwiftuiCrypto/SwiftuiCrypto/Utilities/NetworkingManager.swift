ed8991e2879c189226cf48594933d05209aa074f
Hamp <yongbeomkwak@gmail.com>
2023-04-28 10:39:33 +0900

    :sparkles: :: 에러 핸들링

--- SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Utilities/NetworkingManager.swift
+++ SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Utilities/NetworkingManager.swift
@@ -10,21 +10,43 @@ import Combine
 
 class NetworkingManager {
     
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url:URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+                
+            case .badURLResponse(url: let url):
+                return "❌ Bad Response from URL: \(url)"
+            case .unknown:
+                return "[⚠️] Unknown error occured"
+            }
+        }
+        
+    }
+    
+    
     static func download(url:URL) -> AnyPublisher<Data,Error>  {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap({ output -> Data in
-                guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 &&
-                        response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                
-                return output.data
-            })
+            .tryMap({ try handleURLResponse(output: $0,url: url) })
             .receive(on:DispatchQueue.main)
             .eraseToAnyPublisher()
         
     }
+    
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output,url:URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 &&
+                response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        
+        return output.data
+        
+    }
+    
     
     static func handleCompletion(completion: Subscribers.Completion<Error>) {
         
