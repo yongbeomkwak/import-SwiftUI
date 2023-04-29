2d8592198638ef3b705482aaf422d09da7a014a7
Hamp <yongbeomkwak@gmail.com>
2023-04-29 22:56:20 +0900

    :page_facing_up: :: 세이프 Area 정리

--- SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -17,11 +17,16 @@ class HomeViewModel:ObservableObject {
     
     @Published var isLoading: Bool = false
     @Published var searchText: String = ""
+    @Published var sortOption: SortOption = .holdings
     
     private let coinDataService = CoinDataService()
     private let marketDataService = MarketDataService()
     private let portfolioDataService = PortfolioDataService()
     private var cancellables = Set<AnyCancellable> ()
+    
+    enum SortOption {
+        case rank, rankReversed, holdings, holdingsReversed, price , priceReversed
+    }
 
     
     init(){
