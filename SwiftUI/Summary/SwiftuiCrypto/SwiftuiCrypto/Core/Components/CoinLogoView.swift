1f791013f92e6460ee7e46e0f0a36d41545107a0
Hamp <yongbeomkwak@gmail.com>
2023-04-29 17:14:33 +0900

    :lipstick: :: 코인로고 뷰

--- SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Core/Components/CoinLogoView.swift
+++ SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Core/Components/CoinLogoView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct CoinLogoView: View {
+    
+    let coin: CoinModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            CoinImageView(coin: coin)
+                .frame(width: 50,height: 50)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundColor(.theme.accent)
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
+            Text(coin.name)
+                .font(.caption)
+                .foregroundColor(.theme.secondaryText)
+                .lineLimit(2)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
 struct CoinLogoView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinLogoView()
+        CoinLogoView(coin: dev.coin)
+            .previewLayout(.sizeThatFits)
     }
 }
