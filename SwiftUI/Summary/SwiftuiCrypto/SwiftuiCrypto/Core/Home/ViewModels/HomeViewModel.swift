0fb45138be24b692e64a11dd7db0c171285a906f
Hamp <yongbeomkwak@gmail.com>
2023-04-29 22:16:39 +0900

    :zap: ::  코드 축약

--- SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -44,6 +44,7 @@ class HomeViewModel:ObservableObject {
         
         //update marketData
         marketDataService.$marketData
+            .combineLatest($portfolioCoins)
             .map(mapGlobalMarketData)
             .sink { [weak self] result in
                 guard let self = self else {return}
@@ -56,16 +57,7 @@ class HomeViewModel:ObservableObject {
         
         $allCoins
             .combineLatest(portfolioDataService.$savedEntities)
-            .map{ (coinModels,PortfolioEntities)  -> [CoinModel] in
-                
-                coinModels
-                    .compactMap { (coin) -> CoinModel?  in
-                        guard let entity = PortfolioEntities.first(where: {$0.coinID == coin.id}) else {
-                            return nil
-                        }
-                        return coin.updateHoldings(amount: entity.amount)
-                    }
-            }
+            .map(mapAllCoinsToPortfolioCoins)
             .sink { [weak self] coins in
                 guard let self else {return}
                 
@@ -97,7 +89,20 @@ class HomeViewModel:ObservableObject {
         
     }
     
-    private func mapGlobalMarketData(marketDataModel:MarketDataModel?) -> [StatisticModel] {
+    private func mapAllCoinsToPortfolioCoins(allCoins:[CoinModel], portfolioEntities: [PortfolioEntity]) -> [CoinModel] {
+        
+        allCoins.compactMap({ (coin) -> CoinModel? in
+            
+            guard let entity = portfolioEntities.first(where: {$0.coinID == coin.id}) else {
+                return nil
+            }
+            
+            return coin.updateHoldings(amount: entity.amount)
+            
+        })
+    }
+    
+    private func mapGlobalMarketData(marketDataModel:MarketDataModel?,portfolioCoins:[CoinModel]) -> [StatisticModel] {
         var stats: [StatisticModel] = []
         
         guard let data = marketDataModel else {
@@ -108,7 +113,22 @@ class HomeViewModel:ObservableObject {
         
         let volumn =  StatisticModel(title: "24h Volumn", value: data.volume)
         let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
-        let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00",percentageChange: 0)
+        
+        
+        let portfolioValue = portfolioCoins.map({$0.currentHoldingsValue}).reduce(0,+)
+        
+        let previousValue = portfolioCoins.map { (coin) -> Double in
+            let currentValue = coin.currentHoldingsValue
+            let percentChange = coin.priceChangePercentage24H! / 100
+            let previousValue = currentValue / (1+percentChange)
+            return previousValue
+        }
+        .reduce(0, +)
+        
+        let percentageChange = ((portfolioValue - previousValue) / previousValue) * 100
+        
+        let portfolio = StatisticModel(title: "Portfolio Value", value: portfolioValue.asCurrencyWith2Decimals() ,percentageChange: percentageChange)
+        
         
         stats.append(contentsOf:
             [marketCap,volumn,btcDominance,portfolio]
