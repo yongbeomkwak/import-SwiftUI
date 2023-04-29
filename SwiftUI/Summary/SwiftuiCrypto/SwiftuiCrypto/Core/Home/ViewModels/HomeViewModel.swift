0f5213bb3cf52bd39b1a283b9bba1f7b1dd46608
Hamp <yongbeomkwak@gmail.com>
2023-04-29 15:16:41 +0900

    :lipstick: :: 헤더 완료

--- SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -10,19 +10,15 @@ import Combine
 
 class HomeViewModel:ObservableObject {
     
-    @Published  var statistics: [StatisticModel] = [
-        StatisticModel(title: "Title", value: "Value",percentageChange: 1),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value",percentageChange: -7)
-    ]
+    @Published  var statistics: [StatisticModel] = []
     
     @Published var allCoins:[CoinModel] = []
     @Published var portfolioCoins:[CoinModel] = []
     
     @Published var searchText: String = ""
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
     private var cancellables = Set<AnyCancellable> ()
 
     
@@ -32,14 +28,26 @@ class HomeViewModel:ObservableObject {
     
     func addSubscribers() {
         
+        //update allCoins
         $searchText
-            .combineLatest(dataService.$allCoins) // 실제 전체 코인
+            .combineLatest(coinDataService.$allCoins) // 실제 전체 코인
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map(fillterCoins)
             .sink { [weak self] coins in
                 guard let self else {return}
                 
                 self.allCoins = coins // 필터링 걸러진 코인
+            }
+            .store(in: &cancellables)
+        
+        
+        //update marketData
+        marketDataService.$marketData
+            .map(mapGlobalMarketData)
+            .sink { [weak self] result in
+                guard let self = self else {return}
+                
+                self.statistics = result
             }
             .store(in: &cancellables)
     }
@@ -59,6 +67,26 @@ class HomeViewModel:ObservableObject {
             
         })
         
+    }
+    
+    private func mapGlobalMarketData(marketDataModel:MarketDataModel?) -> [StatisticModel] {
+        var stats: [StatisticModel] = []
+        
+        guard let data = marketDataModel else {
+            return stats
+        }
+        
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap,percentageChange: data.marketCapChangePercentage24HUsd)
+        
+        let volumn =  StatisticModel(title: "24h Volumn", value: data.volume)
+        let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00",percentageChange: 0)
+        
+        stats.append(contentsOf:
+            [marketCap,volumn,btcDominance,portfolio]
+        )
+        
+        return stats
     }
     
 }
---
+++ SwiftUI/Summary/SwiftuiCrypto/SwiftuiCrypto/Services/MarketDataService.swift
@@ -0,0 +1,42 @@
+//
+//  CoinDataService.swift
+//  SwiftuiCrypto
+//
+//  Created by yongbeomkwak on 2023/04/25.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel? = nil
+    var marketDataSubscription: AnyCancellable?
+    
+    
+    init(){
+        getCoins()
+    }
+    
+    
+    private func getCoins(){
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else {return}
+        
+    
+        marketDataSubscription = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] globalData in
+                guard let self else  {return}
+                self.marketData = globalData.data
+                self.marketDataSubscription?.cancel()
+                
+            })
+           
+        
+        
+    }
+    
+    
+    
+}
